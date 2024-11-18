6d9ac7051f2b764a4a6d3d3dc6112420d4fdbe32
guatom999 <35306045+guatom999@users.noreply.github.com>
2024-11-18 17:18:05 +0700

    fix type

--- entities/playercoin.go
+++ entities/playercoin.go
@@ -4,7 +4,7 @@ import "time"
 
 type (
 	PlayerCoin struct {
-		ID        uint64    `gorm:"primaryKey;autoIncrement;`
+		ID        uint64    `gorm:"primaryKey;autoIncrement;"`
 		PlayerId  string    `gorm:"type:varchar(64);not null;"`
 		Amount    int64     `gorm:"not null;"`
 		CreatedAt time.Time `gorm:"not null;autoCreateTime;"`
