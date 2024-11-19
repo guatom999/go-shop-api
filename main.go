a2ce72d8f2071e3f4e244ea5b11b5ba75ad9e981
guatom999 <35306045+guatom999@users.noreply.github.com>
2024-11-19 13:33:26 +0700

    add server

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"github.com/guatom999/go-shop-api/config"
+	"github.com/guatom999/go-shop-api/databases"
+	"github.com/guatom999/go-shop-api/server"
+)
+
 func main() {
 
+	conf := config.GetConfig()
+	db := databases.NewPostgresDatabase(conf.Database)
+	server := server.NewEchoServer(conf, db.ConnectDatabase())
+
+	server.Start()
 }
--- server/server.go
+++ server/server.go
@@ -1,1 +1,59 @@
 package server
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+
+	"github.com/guatom999/go-shop-api/config"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+	"gorm.io/gorm"
+)
+
+type (
+	echoServer struct {
+		app  *echo.Echo
+		db   *gorm.DB
+		conf *config.Config
+	}
+)
+
+var (
+	server *echoServer
+	once   sync.Once
+)
+
+func NewEchoServer(conf *config.Config, db *gorm.DB) *echoServer {
+	echoApp := echo.New()
+	echoApp.Logger.SetLevel(log.DEBUG)
+
+	once.Do(func() {
+		server = &echoServer{
+			app:  echoApp,
+			db:   db,
+			conf: conf,
+		}
+	})
+
+	return server
+}
+
+func (s *echoServer) Start() {
+
+	s.app.GET("/v1/health", s.healthCheck)
+
+	s.httpListening()
+}
+
+func (s *echoServer) httpListening() {
+	serverUrl := fmt.Sprintf(":%d", s.conf.Server.Port)
+
+	if err := s.app.Start(serverUrl); err != nil && err != http.ErrServerClosed {
+		s.app.Logger.Fatalf("Error: %s", err.Error())
+	}
+}
+
+func (s *echoServer) healthCheck(c echo.Context) error {
+	return c.String(http.StatusOK, "OK")
+}
