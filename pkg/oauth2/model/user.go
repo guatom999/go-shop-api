2f88237cc576c000264d7337ef4a9794f0a73160
guatom999 <35306045+guatom999@users.noreply.github.com>
2024-11-24 13:51:53 +0700

    fix custom err res

--- pkg/oauth2/model/user.go
+++ pkg/oauth2/model/user.go
@@ -3,7 +3,7 @@ package model
 type UserInfo struct {
 	ID            string `json:"id"`
 	Email         string `json:"email"`
-	VerifiedEmail string `json:"verified_email"`
+	VerifiedEmail bool   `json:"verified_email"`
 	Name          string `json:"name"`
 	GivenName     string `json:"given_name"`
 	FamilyName    string `json:"family_name"`
